60834d47c5f9e967b0089371c951ef32c1554b72
amitYadavDev <amit8955461017@gmail.com>
2024-02-03 20:46:48 +0530

    adding data

--- app/src/main/java/amitapps/media/swipeapp/ListingProductsFragment.kt
+++ app/src/main/java/amitapps/media/swipeapp/ListingProductsFragment.kt
@@ -13,6 +13,7 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import android.widget.Toast
+import androidx.appcompat.widget.SearchView
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.Observer
 import androidx.recyclerview.widget.LinearLayoutManager
